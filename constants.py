5277d42af79a74b6f390af267d4b6746b33fa87d
Arthur Viana <46693700+VianaArthur@users.noreply.github.com>
2021-09-29 19:36:00 -0300

    Add constants.py

---
+++ constants.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+URL = "https://web.whatsapp.com/"
+GOOGLE_CHROME_BIN = ""  # Path to installed Google Chrome Application
+CHROME_DRIVER = "./chromedriver"
+FILES_FOLDER = f"{Path().absolute()}/files"
+SENT_FILES_FOLDER = f"{Path().absolute()}/sent_files"
